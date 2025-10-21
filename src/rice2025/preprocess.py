4a3054db237e2dd0668b8d0d18b3c7a6003ec027
Lucas He <lucas.he.2003@gmail.com>
2025-10-21 16:29:23 -0500

    implemented normalize actually clear

--- src/rice2025/preprocess.py
+++ src/rice2025/preprocess.py
@@ -6,23 +6,56 @@ Inputs:
     - data: a NumPy array of any shape
 """
 def scale(data):
+    # convert to np array
+    data = np.array(data, np.float64)
     # check for empty data
     if data.size == 0:
         return data
     # handle 1D array
     if len(data.shape) == 1:
         min, max = np.min(data), np.max(data)
-        return (data - min) / (max - min)
+        if min - max == 0: # handle div by 0
+            return np.zeros_like(data)
+        else:
+            return (data - min) / (max - min)
     # handle other shapes
     else:
         scaled = np.zeros_like(data, np.float64)
         for i in range(data.shape[1]):
             min, max = np.min(data[:, i]), np.max(data[:, i])
-            scaled[:, i] = (data[:, i] - min) / (max - min)
+            if min - max == 0: # handle div by 0
+                scaled[:, i] = np.zeros_like(data[:, i])
+            else:
+                scaled[:, i] = (data[:, i] - min) / (max - min)
         return scaled
 
-def normalize():
-    return
 
+"""
+Normalizes the input data using z-score standardization. 
+Inputs:
+    - data: a NumPy array of any shape
+"""
+def normalize(data):
+    # convert to np array
+    data = np.array(data, np.float64)
+    # check for empty data
+    if data.size == 0:
+        return data
+    # handle 1D array
+    if len(data.shape) == 1:
+        mean, std = np.mean(data), np.std(data)
+        if std == 0: # handle div by 0
+            std = 1
+        return (data - mean) / std
+    # handle other shapes
+    else:
+        normalized = np.zeros_like(data, np.float64)
+        for i in range(data.shape[1]):
+            mean, std = np.mean(data[:, i]), np.std(data[:, i])
+            if std == 0: # handle div by 0
+                std = 1
+            normalized[:, i] = (data[:, i] - mean) / std
+        return normalized
+    
 def train_test_split():
     return
--- tests/unit/test_preprocess.py
+++ tests/unit/test_preprocess.py
@@ -11,6 +11,7 @@ two_d = np.array([[0, 2, 3],
                      [5, 4, 3],
                      [10, 10, 9]])
 negative = np.array([-5, 0, 5])
+rep = np.array([5, 5, 5])
 
 """
 Testing scale function.
@@ -36,3 +37,38 @@ def test_scale_negative():
     scaled = prep.scale(negative)
     exp = np.array([0.0, 0.5, 1.0])
     np.testing.assert_equal(scaled, exp)
+
+def test_scale_rep():
+    scaled = prep.scale(rep)
+    exp = np.array([0.0, 0.0, 0.0])
+    np.testing.assert_equal(scaled, exp)
+
+"""
+Test normalize function.
+"""
+def test_normalize_empty():
+    normalized = prep.normalize(empty)
+    exp = np.array([])
+    np.testing.assert_equal(normalized, exp)
+
+def test_normalize_1d():
+    normalized = prep.normalize(one_d)
+    exp = np.array([-1.22474487, 0.0, 1.22474487])
+    np.testing.assert_allclose(normalized, exp, 1e-8)
+
+def test_normalize_2d():
+    normalized = prep.normalize(two_d)
+    exp = np.array([[-1.22474487, -0.98058068, -0.70710678],
+                    [ 0.        , -0.39223227, -0.70710678],
+                    [ 1.22474487,  1.37281295,  1.41421356]])
+    np.testing.assert_allclose(normalized, exp, 1e-8)
+
+def test_normalize_negative():
+    normalized = prep.normalize(negative)
+    exp = np.array([-1.22474487, 0.0, 1.22474487])
+    np.testing.assert_allclose(normalized, exp, 1e-8)
+
+def test_normalize_rep():
+    normalized = prep.normalize(rep)
+    exp = np.array([0.0, 0.0, 0.0])
+    np.testing.assert_allclose(normalized, exp, 1e-8)
