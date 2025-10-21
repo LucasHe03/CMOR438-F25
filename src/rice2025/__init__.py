e15303fe00aaefc3175f724d89c756b3ef6ca48d
Lucas He <lucas.he.2003@gmail.com>
2025-10-21 14:23:58 -0500

    init file

--- src/rice2025/__init__.py
+++ src/rice2025/__init__.py
@@ -1,2 +1,23 @@
-__all__ = ["add"]
-from .basic_functions import add
+import os
+import glob
+import importlib
+
+# get directory
+dir = os.path.dirname(__file__)
+
+# get all .py files
+files = glob.glob(os.path.join(dir, "*.py"))
+modules = [
+    os.path.splitext(os.path.basename(f))[0]
+    for f in files
+    if not f.endswith("__init__.py")
+]
+
+# import each module
+for module in modules:
+    importlib.import_module(f".{module}", package=__name__)
+
+__all__ = modules
+
+# success message (for testing)
+# print("rice2025 package imported successfully")
