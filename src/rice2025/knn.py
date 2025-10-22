c445030a278ff97d0fae0fac2c26ef1c533bf15c
Lucas He <lucas.he.2003@gmail.com>
2025-10-22 17:16:58 -0500

    knn skeleton

--- src/rice2025/knn.py
+++ src/rice2025/knn.py
@@ -1,3 +1,11 @@
+import numpy as np
+
 class KNN:
-    def __init__():
+    def __init__(self):
+        return
+    
+    def fit(self):
+        return
+    
+    def predict(self):
         return
