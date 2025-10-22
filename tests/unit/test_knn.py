322fa63861f6c411d930abe4dd55559648a8fc9e
Lucas He <lucas.he.2003@gmail.com>
2025-10-22 18:09:35 -0500

    tested fit

--- tests/unit/test_knn.py
+++ tests/unit/test_knn.py
@@ -20,3 +20,43 @@ def test_init_zero():
 def test_init_negative():
     with pytest.raises(ValueError):
         model = knn.KNN(-1)
+
+"""
+Test fit function
+"""
+def test_fit_success():
+    model = knn.KNN()
+    x = [[1, 2], [3, 4], [5, 6], [7, 8]]
+    y = [0, 1, 0, 1]
+    model.fit(x, y)
+    np.testing.assert_array_equal(model.x_train, np.array(x, dtype=np.float64))
+    np.testing.assert_array_equal(model.y_train, np.array(y))
+
+def test_fit_success_classify():
+    model = knn.KNN()
+    x = [[1, 2], [3, 4], [5, 6], [7, 8]]
+    y = ["cat", "dog", "cat", "dog"]
+    model.fit(x, y)
+    np.testing.assert_array_equal(model.x_train, np.array(x, dtype=np.float64))
+    np.testing.assert_array_equal(model.y_train, np.array(y))
+
+def test_fit_mismatched_lengths():
+    model = knn.KNN()
+    x = [[1, 2], [3, 4]]
+    y = [0]
+    with pytest.raises(ValueError):
+        model.fit(x, y)
+
+def test_fit_empty():
+    model = knn.KNN()
+    x = []
+    y = []
+    with pytest.raises(ValueError):
+        model.fit(x, y)
+
+def test_fit_k_too_large():
+    model = knn.KNN(5)
+    x = [[1, 2], [3, 4]]
+    y = [0, 1]
+    with pytest.raises(ValueError):
+        model.fit(x, y)
